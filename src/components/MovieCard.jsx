14ae90bcd3bf1c3003b36d709c01ccbe32fc7c58
Anik Das <onikd08@gmail.com>
2024-01-26 02:50:03 +0200

    Implement MovieDetails modal

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,29 +1,57 @@
 /* eslint-disable react/prop-types */
+import { useContext, useState } from "react";
 import getImageUrl from "../utils/getImageUrl";
 import Rating from "./Rating";
+import MovieDetailsModal from "./MovieDetailsModal";
+import tag from "../assets/tag.svg";
+import { CartContext } from "../providers/CartContextProvider";
 
 const MovieCard = ({ movie }) => {
   const { cover, title, price, rating, genre } = movie;
+  const [showMovieModal, setShowMovieModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(false);
+
+  const { handleAddToCart } = useContext(CartContext);
+
+  const handleModalClose = () => {
+    setSelectedMovie(null);
+    setShowMovieModal(false);
+  };
+
+  const handleMovieSelection = (movie) => {
+    setSelectedMovie(movie);
+    setShowMovieModal(true);
+  };
+
   return (
-    <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-      <img
-        className="w-full object-cover"
-        src={getImageUrl(cover)}
-        alt={title}
-      />
-      <figcaption className="pt-4">
-        <h3 className="text-xl mb-1">{title}</h3>
-        <p className="text-[#575A6E] text-sm mb-2">{genre}</p>
-        <Rating rating={rating} />
-        <a
-          className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-          href="#"
-        >
-          <img src="./assets/tag.svg" alt="" />
-          <span>${price} | Add to Cart</span>
-        </a>
-      </figcaption>
-    </figure>
+    <>
+      {showMovieModal && (
+        <MovieDetailsModal movie={selectedMovie} onClose={handleModalClose} />
+      )}
+      <figure
+        onClick={() => handleMovieSelection(movie)}
+        className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl cursor-pointer"
+      >
+        <img
+          className="w-full object-cover"
+          src={getImageUrl(cover)}
+          alt={title}
+        />
+        <figcaption className="pt-4">
+          <h3 className="text-xl mb-1">{title}</h3>
+          <p className="text-[#575A6E] text-sm mb-2">{genre}</p>
+          <Rating rating={rating} />
+          <a
+            className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+            href="#"
+            onClick={(e) => handleAddToCart(e, movie)}
+          >
+            <img src={tag} alt="tag" />
+            <span>${price} | Add to Cart</span>
+          </a>
+        </figcaption>
+      </figure>
+    </>
   );
 };
 
